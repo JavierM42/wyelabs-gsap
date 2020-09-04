42237fe6dd147e05789114abaf5df6e3d587e4a1
JavierM42 <Javier Morales>
2020-09-04 16:07:49 -0300

    bonus track: hiding and showing with vue, setup

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <div id="app" class="p-8">
-    <div class="flex space-x-2">
-      <div class="square w-16 h-16 bg-red-600" />
-      <div class="circle w-16 h-16 bg-blue-600 rounded-full" />
+    <div class="h-16">
+      <div v-if="showSquare" class="square w-16 h-16 bg-red-600" />
     </div>
     <div class="my-4 space-x-4">
       <button
-        @click="animate"
+        @click="showSquare = !showSquare"
         class="px-4 py-1 border border-red-500 bg-red-200 rounded-full hover:shadow-lg font-medium text-red-900"
       >
-        Animate me
+        Toggle the square
       </button>
     </div>
   </div>
@@ -22,22 +21,8 @@ export default {
   name: 'App',
   data() {
     return {
-      tl: gsap.timeline()
+      showSquare: true
     };
-  },
-  mounted() {
-    this.tl.to('.square', { duration: .2, x: 300 });
-    this.tl.addLabel('squareOnTopRight');
-    this.tl.to('.square', { duration: .2, y: 300 });
-    this.tl.to('.square', { duration: .2, x: 0 });
-    this.tl.to('.square', { duration: .2, y: 0 });
-
-    this.tl.to('.circle', { duration: .2, x: 300 }, 'squareOnTopRight');
-    this.tl.to('.circle', { duration: .2, y: 300 }, '>');
-    this.tl.to('.circle', { duration: .2, x: 0 }, '>');
-    this.tl.to('.circle', { duration: .2, y: 0 }, '>');
-
-    this.tl.pause();
   },
   methods: {
     animate() {
