96717e73da658dc05c29646d424018071169b2a8
JavierM42 <Javier Morales>
2020-09-04 15:57:14 -0300

    bad timeline movement

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,12 @@
 <template>
   <div id="app" class="p-8">
-    <div class="flex space-x-2">
-      <div class="square w-16 h-16 bg-red-600" />
-      <div class="square w-16 h-16 bg-red-600" />
-      <div class="square w-16 h-16 bg-red-600" />
-      <div class="square w-16 h-16 bg-red-600" />
-    </div>
+    <div class="square w-16 h-16 bg-red-600" />
     <div class="my-4 space-x-4">
       <button
         @click="animate"
         class="px-4 py-1 border border-red-500 bg-red-200 rounded-full hover:shadow-lg font-medium text-red-900"
       >
         Animate me
-      </button>
-      <button
-        @click="animateBack"
-        class="px-4 py-1 border border-blue-500 bg-blue-200 rounded-full hover:shadow-lg font-medium text-blue-900"
-      >
-        Take me back
       </button>
     </div>
   </div>
@@ -30,10 +19,10 @@ export default {
   name: 'App',
   methods: {
     animate() {
-      gsap.to('.square', { duration: 1, opacity: 0.5, x: 300, rotation: 360, scale: 2, ease: 'back.inOut', stagger: 0.1 });
-    },
-    animateBack() {
-      gsap.to('.square', { duration: 1, opacity: 1, x: 0, rotation: 0, scale: 1, ease: 'bounce.out', stagger: 0.5 });
+      gsap.to('.square', { duration: .2, x: 300 });
+      gsap.to('.square', { duration: .2, x: 300, y: 300, delay: .2 });
+      gsap.to('.square', { duration: .2, x: 0, y: 300, delay: .4 });
+      gsap.to('.square', { duration: .2, x: 0, y: 0, delay: .6 });
     }
   }
 }
